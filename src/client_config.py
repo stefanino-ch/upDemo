2384274a85a879bdcf46a974185dc3092238e6a2
stefan <mail@sfeuz.ch>
2020-07-16 19:41:27 +0200

    Added a seperat server for easier testing.

--- src/client_config.py
+++ src/client_config.py
@@ -1,7 +1,10 @@
+import up
+
 class ClientConfig(object):
     PUBLIC_KEY = 'v4aGoCFIJ0Y3UaUz9B0cUoQImOaGYtR6cHBlqoqeZes'
     APP_NAME = 'upDemo'
     COMPANY_NAME = 'Laboratory'
     HTTP_TIMEOUT = 30
     MAX_DOWNLOAD_RETRIES = 1
-    UPDATE_URLS = ['']
+    UPDATE_URLS = ['http://127.0.0.1:50382']
+    APP_VERSION = up.__version__
--- src/upDemo.py
+++ src/upDemo.py
@@ -1,57 +1,151 @@
 from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QMessageBox
 
 import sys
 import up
 import time
+import argparse
+
+import logging
+logger = logging.getLogger(__name__)
+STDERR_HANDLER = logging.StreamHandler(sys.stderr)
+STDERR_HANDLER.setFormatter(logging.Formatter(logging.BASIC_FORMAT))
 
 # pyupdater
 from pyupdater.client import Client
-from config import ClientConfig
+# Be careful here. This import work only if "pyupdater init" was run before.
+from client_config import ClientConfig
 
 class MainWindow(QtWidgets.QMainWindow):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
+        self.buildWindow()
+    
+    def checkForUpdates(self):
+        '''
+        Checks if there are updates available
+        @return: True if there are updates
+        '''
+        sys.stdout.write('Check for Updates. Current version %s\n' % up.__version__)
+        
+        client = Client(ClientConfig(), refresh=True, progress_hooks=[self.log_progress])
+        client.refresh()
+        
+        app_update = client.update_check( ClientConfig.APP_NAME,
+                                          ClientConfig.APP_VERSION,
+                                          channel='stable')
+        
+        if app_update: 
+            sys.stdout.write('Uptdates found\n')
+            return True
+        else:
+            return False
+            sys.stdout.write('NO uptdates found\n')
+            
+    def downloadNewVersion(self):
+        ''' Checks if there is a new version. Does immediately a download if so. 
+        '''
+        client = Client(ClientConfig(), refresh=True, progress_hooks=[self.log_progress])
+        client.refresh()
+        
+        app_update = client.update_check( ClientConfig.APP_NAME,
+                                          ClientConfig.APP_VERSION,
+                                          channel='stable')
+        if app_update:
+            sys.stdout.write('Uptdates found\n')
+            if hasattr(sys, "frozen"):
+                sys.stdout.write('App is frozen\n')
+                
+                downloaded = app_update.download()
+                sys.stdout.write('App Update downloaded\n')
+                if downloaded:
+                    sys.stdout.write('Extracting update and restarting...')
+                    time.sleep(10)
+                    app_update.extract_restart()
+            else:
+                sys.stdout.write('App is NOT frozen\n')
+        else:
+            sys.stdout.write('NO uptdates found')
+    
+    def buildWindow(self):
         self.setWindowTitle("Hello World")
         l = QtWidgets.QLabel("My simple app.")
         l.setMargin(10)
         self.setCentralWidget(l)
         self.show()
-        
+        l.setText('checking for updates')
+        updateAv = self.checkForUpdates()
 
-def print_status_info(info):
-    total = info.get(u'total')
-    downloaded = info.get(u'downloaded')
-    status = info.get(u'status')
-    sys.stdout.write(downloaded, total, status)
+        if updateAv:
+            msgBox = QMessageBox()
+            msgBox.setWindowTitle("Update available")
+            msgBox.setText("New version available. \n\nWanna update\nPress Cancel to abort.")
+            msgBox.setIcon(QMessageBox.Warning)        
+            msgBox.setStandardButtons(QMessageBox.Ok | QMessageBox.Cancel)
+            answer = msgBox.exec()            
+            
+            if answer == QMessageBox.Ok:
+                # User wants to update
+                l.setText('downloading....')
+                self.downloadNewVersion()
+            else: 
+                l.setText('Update aborted')
+        else: 
+            l.setText('No updates found')
+
+    def log_progress(data):
+        # Progress hooks get passed a dict with the below keys.
+        # total: total file size
+        # downloaded: data received so far
+        # status: will show either downloading or finished
+        # percent_complete: Percentage of file downloaded so far
+        # time: Time left to complete download
+        # {'total': 40431556, 'downloaded': 40431556, 'status': 'finished', 'percent_complete': '100.0', 'time': '00:00'}
+        dataTot = data['total']
+        downlded = data['downloaded']
+        percCompl = data['percent_complete']
+        timeRem = data['time']
+        sys.stdout.write(data)
+        sys.stdout.write('Data tot:      ' + dataTot +'\n' )
+        sys.stdout.write('Downloaded:    ' + downlded +'\n' )
+        sys.stdout.write('Perc complete: ' + percCompl +'\n' )
+        sys.stdout.write('Time remaining:' + timeRem  +'\n' )
+    
+    
+def ParseArgs(argv):
+    """
+    Parse command-line args.
+    """
+    usage = ("%(prog)s [options]\n")
+    parser = argparse.ArgumentParser(usage=usage)
+    parser.add_argument("--debug", help="increase logging verbosity", action="store_true")
+    parser.add_argument("--version", action='store_true', help="displays version")
+    return parser.parse_args(argv[1:])
+
+def InitializeLogging(debug=False):
+    """
+    Initialize logging.
+    """
+    logger.addHandler(STDERR_HANDLER)
+    if debug:
+        level = logging.DEBUG
+    else:
+        level = logging.INFO
+    logger.setLevel(level)
+    logging.getLogger("upDemo").addHandler(STDERR_HANDLER)
+    logging.getLogger("upDemo").setLevel(level)
+
+
     
 if __name__ == '__main__':
     
-    # pyupdater
-    print("version %s\n" % up.__version__)
-    client = Client(ClientConfig())
-    client.refresh()
-    client.add_progress_hook(print_status_info)
+    args = ParseArgs(sys.argv)
+    InitializeLogging(args.debug)
     
+    sys.stdout.write('Initialized...\n')
     
-    app_update = client.update_check( ClientConfig.APP_NAME,
-                                      ClientConfig.APP_VERSION,
-                                      channel='stable')
-    if app_update:
-        print('app_update')
-        if hasattr(sys, "frozen"):
-            downloaded = app_update.download()
-            if downloaded:
-                sys.stdout.write('Extracting update and restarting...')
-                time.sleep(10)
-                app_update.extract_restart()
-        else:
-            sys.stdout.write('not frozen')
-    else:
-        print('sorry, no app_update')
-    
-    # normal app
     app = QtWidgets.QApplication(sys.argv)
     w = MainWindow()
     app.exec()
